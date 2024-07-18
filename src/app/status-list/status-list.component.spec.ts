6ebaa9271d7f4abcc45b73d776c034e52b05ff5f
Cesa <Cesa263@gmail.com>
2024-07-18 15:54:58 +0700

    add cause for title

--- src/app/status-list/status-list.component.spec.ts
+++ src/app/status-list/status-list.component.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatusListComponent } from './status-list.component';
+import { EmployeeStatusService } from '../employee-status.service';
+import jasmine from 'jasmine';
 
 describe('StatusListComponent', () => {
+  let service: EmployeeStatusService
   let component: StatusListComponent;
   let fixture: ComponentFixture<StatusListComponent>;
 
@@ -12,6 +15,7 @@ describe('StatusListComponent', () => {
     })
       .compileComponents();
 
+    service = TestBed.inject(EmployeeStatusService)
     fixture = TestBed.createComponent(StatusListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,5 +28,11 @@ describe('StatusListComponent', () => {
   it('should have title', () => {
     const compiled = fixture.nativeElement as HTMLElement
     expect(compiled.querySelector(".page-title")?.textContent).toContain("Employee Status List")
+  })
+
+  it('should render as many rows as employee status items', async () => {
+    const compiled = fixture.nativeElement
+    const res = await service.getEmployeeStatuses()
+    expect(compiled.querySelectorAll('.dx-data-row').length).toBe(res.length)
   })
 });
